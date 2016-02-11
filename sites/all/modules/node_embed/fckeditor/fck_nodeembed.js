52f95b9a8e3930c55be392cc2ea746c459585ee2
Ubuntu <ubuntu@ip-172-31-36-0.us-west-2.compute.internal>
2016-02-11 23:00:11 +0000

    added book module

--- sites/all/modules/node_embed/fckeditor/fck_nodeembed.js
+++
@@ -1,8 +0,0 @@
-Drupal.behaviors.node_embed = function(context) {  
-  $('table.views-view-grid tr td', context).click(function(){ 
-    $('.active-cell .views-field-nid span.field-content').attr("id", 'inactive-nid');
-    $('table.views-view-grid tr td', context).removeClass('active-cell'); 
-    $(this).addClass('active-cell'); 
-    $('.active-cell .views-field-nid span.field-content').attr("id", 'active-nid');
-  }); 
-}
